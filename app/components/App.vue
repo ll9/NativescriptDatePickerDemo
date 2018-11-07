32bbcfb833bbb8232392846f5d0fb0f784185ef4
ll9 <llwierer@web.de>
2018-11-07 21:01:36 +0100

    datepicker modal (close fails

---
+++ app/components/App.vue
@@ -0,0 +1,61 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!" android:flat="true"/>
+        <TabView android:tabBackgroundColor="#53ba82"
+                 android:tabTextColor="#c4ffdf"
+                 android:selectedTabTextColor="#ffffff"
+                 androidSelectedTabHighlightColor="#ffffff">
+            <TabViewItem title="Tab 1">
+                <StackLayout>
+                    <TextField hint="datetime" keyboardType="datetime"></TextField>
+                    <TextField hint="phone" keyboardType="phone"></TextField>
+                    <TextField hint="number" keyboardType="number"></TextField>
+                    <TextField hint="url" keyboardType="url"></TextField>
+                    <TextField hint="email" keyboardType="email"></TextField>
+                    <TextField hint="custDate" @tap="openDatePicker"></TextField>
+                </StackLayout>
+            </TabViewItem>
+            <TabViewItem title="Tab 2">
+                <GridLayout columns="*" rows="*">
+                    <Label class="message" text="Tab 2 Content" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+            <TabViewItem title="Tab 3">
+                <GridLayout columns="*" rows="*">
+                    <Label class="message" text="Tab 3 Content" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+        </TabView>
+    </Page>
+</template>
+
+<script>
+import DatePickerModal from './DatePickeModal';
+
+export default {
+  data() {
+    return {
+      msg: "Hello World!"
+    };
+  },
+  methods: {
+    openDatePicker() {
+        this.$showModal(DatePickerModal)
+    }
+  }
+};
+</script>
+
+<style scoped>
+ActionBar {
+  background-color: #53ba82;
+  color: #ffffff;
+}
+
+.message {
+  vertical-align: center;
+  text-align: center;
+  font-size: 20;
+  color: #333333;
+}
+</style>
---
+++ app/components/DatePickeModal.vue
@@ -0,0 +1,16 @@
+<template>
+    <Page>
+    <StackLayout>
+    </StackLayout>
+    </Page>
+</template>
+
+<script>
+export default {
+  data() {},
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
