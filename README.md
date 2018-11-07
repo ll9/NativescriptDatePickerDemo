54be5cf02b2a7c233ae150959345c65cde035d08
ll9 <llwierer@web.de>
2018-11-07 21:18:17 +0100

    better datepicker experience

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+# get correct date from datepicker
+
+first install this plugin: https://www.npmjs.com/package/nativescript-modal-datetimepicker
+
+in order to get the correct date (independent of the timezone) use this:
+
+var now = new Date();
+var todayUTC = new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate()));
+return todayUTC.toISOString().slice(0, 10).replace(/-/g, '');
+
+(see: https://stackoverflow.com/questions/3066586/get-string-in-yyyymmdd-format-from-js-date-object)
+
 # NativeScript-Vue Application
 
 > A native application built with NativeScript-Vue
--- app/components/App.vue
+++ app/components/App.vue
@@ -13,6 +13,7 @@
                     <TextField hint="url" keyboardType="url"></TextField>
                     <TextField hint="email" keyboardType="email"></TextField>
                     <TextField hint="custDate" @tap="openDatePicker"></TextField>
+                    <TextField v-model="date" hint="pluginDatepicker" @tap="openDatePickerPlugin"></TextField>
                 </StackLayout>
             </TabViewItem>
             <TabViewItem title="Tab 2">
@@ -30,17 +31,44 @@
 </template>
 
 <script>
-import DatePickerModal from './DatePickeModal';
+import DatePickerModal from "./DatePickeModal";
+import { ModalDatetimepicker } from "nativescript-modal-datetimepicker";
+
+const picker = new ModalDatetimepicker();
 
 export default {
   data() {
     return {
-      msg: "Hello World!"
+      msg: "Hello World!",
+      date: ""
     };
   },
   methods: {
     openDatePicker() {
-        this.$showModal(DatePickerModal)
+      this.$showModal(DatePickerModal);
+    },
+    openDatePickerPlugin() {
+      picker
+        .pickDate({
+          theme: "light",
+          maxDate: new Date()
+        })
+        .then(result => {
+          // Note the month is 1-12 (unlike js which is 0-11)
+          console.log(
+            "Date is: " + result.day + "-" + result.month + "-" + result.year
+          );
+          var jsdate = new Date(result.year, result.month - 1, result.day);
+          var todayUTC = new Date(
+            Date.UTC(jsdate.getFullYear(), jsdate.getMonth(), jsdate.getDate())
+          );
+          this.date = todayUTC
+            .toISOString()
+            .slice(0, 10)
+        })
+        .catch(error => {
+          console.log("Error: " + error);
+        });
     }
   }
 };
